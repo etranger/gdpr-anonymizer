46e8d924fe1b1a3f35e7c2000c6cc5dd5d93b2d3
SMozhaev <mozhaev.dev@gmail.com>
2022-02-23 12:57:16 +0200

    test 1

--- src/servicces/dataHandlers/testDataHandler.ts
+++ src/servicces/dataHandlers/testDataHandler.ts
@@ -1,236 +1,26 @@
 import { DataHandler } from "../personal-data-parser";
 import parse from "../fileParsers/pdfParser";
+import { TextItem, TextMarkedContent } from "pdfjs-dist/types/src/display/api";
 
-async function process(data: string[]) {
-  type ColTest = (strIndex: number) => boolean;
-
-  const colTests: ColTest[] = [
-    (strIndex) => /^\d{1,2}.\d{1,2}.\d{4}$/.test(data[strIndex]),
-    (strIndex) =>
-      /^.+$/.test(data[strIndex]) &&
-      !colTests[0](strIndex) &&
-      !colTests[2](strIndex),
-    (strIndex) => {
-      const t = /^\d+$/.test(data[strIndex]) && !colTests[3](strIndex);
-      return t;
-    },
-    (strIndex) => {
-      const t = /^\d+$/.test(data[strIndex]) && colTests[2](strIndex - 1);
-      return t;
-    },
-    (strIndex) =>
-      /^.+$/.test(data[strIndex]) &&
-      !colTests[3](strIndex) &&
-      !colTests[5](strIndex),
-    (strIndex) => {
-      const t = /^\d+,?\d{0,}$/.test(data[strIndex]) && !colTests[6](strIndex);
-      return t;
-    },
-    (strIndex) => {
-      const t =
-        /^\d+,?\d{0,}$/.test(data[strIndex]) && colTests[5](strIndex - 1);
-      return t;
-    },
-  ];
-
-  const p = new Promise<string[][]>((resolve) => {
-    const result: string[][] = [];
-
-    const iter = (
-      currentStrIndex: number = 0,
-      currentColIndex: number = 0,
-      colAcc: string[] = [],
-      rowAcc: string[] = []
-    ) =>
-      setTimeout(() => {
-        if (currentStrIndex > data.length - 1) {
-          resolve(result);
-        }
-
-        if (currentColIndex > colTests.length - 1) {
-          result.push(rowAcc);
-          iter(currentStrIndex);
-        } else if (
-          colTests[currentColIndex](currentStrIndex) &&
-          !colTests[
-            currentColIndex < colTests.length - 1 ? currentColIndex + 1 : 0
-          ](currentStrIndex)
-        ) {
-          iter(
-            currentStrIndex + 1,
-            currentColIndex,
-            [...colAcc, data[currentStrIndex]],
-            rowAcc
-          );
-        } else if (!colTests[currentColIndex](currentStrIndex)) {
-          iter(
-            currentStrIndex,
-            currentColIndex + 1,
-            [],
-            [...rowAcc, colAcc.join()]
-          );
-        } else if (
-          colTests[currentColIndex](currentStrIndex) &&
-          colTests[
-            currentColIndex < colTests.length - 1 ? currentColIndex + 1 : 0
-          ](currentStrIndex)
-        ) {
-          iter(currentStrIndex + 1);
-        }
-      }, 0);
-
-    iter();
-  });
-
-  p.then((r) => console.log(r));
-
-  return [];
-}
-
-const splitPdfDataByRows = (pdfData: string[]) => {
-  const pdfDataWithoutSpaces = pdfData.filter(
-    (item: any) => item !== undefined && item !== " " && item !== ""
-  );
-
-  console.log(
-    "Step 2 - delete spaces and undefined values:",
-    pdfDataWithoutSpaces
-  );
-
-  let pdfDataFiltered: string[] = pdfDataWithoutSpaces;
-
-  const pdfDataHasTableOfContent = pdfDataWithoutSpaces.find(
-    (str) => str === "Ostosi tuotetasolla"
-  );
-
-  if (pdfDataHasTableOfContent) {
-    const tableOfContentEndsIndex = pdfDataWithoutSpaces.findIndex(
-      (str) => str === "Summa sis. ALV"
-    );
-
-    pdfDataFiltered = pdfDataWithoutSpaces.slice(
-      tableOfContentEndsIndex > 0 ? tableOfContentEndsIndex + 1 : 0
-    );
-  }
-
-  console.log("Step 3 - delete table of content:", pdfDataFiltered);
-
-  const cleaningSubjects: string[] = [
-    "Päivämäärä",
-    "Kauppa",
-    "Kuittinumero",
-    "Korttinumero",
-    "Tuote",
-    "Määrä",
-    "Summa sis. ALV",
-    "K-Plussa",
-    "Luottamuksellinen",
-  ];
-  const isPaginationNumber = (index: number, array: string[]) => {
-    const isNumber = (value: string) => /^\d{1,2}$/.test(value);
-    const isSlash =
-      array[index] === "/" &&
-      isNumber(array[index - 1]) &&
-      isNumber(array[index + 1]);
-    const isLeftNumber = isNumber(array[index]) && array[index + 1] === "/";
-    const isRightNumber = isNumber(array[index]) && array[index - 1] === "/";
-    const isFullPagination = /^\d{1,2}\/\d{1,2}$/.test(array[index]);
-
-    return isSlash || isLeftNumber || isRightNumber || isFullPagination;
-  };
-  const isDataDate = (value: string) => {
-    const regex = /\d{4}-\d{1,2}-\d{1,2}/;
-    if (regex.test(value)) return true;
-    if (value === "Tiedot ajettu:") return true;
-
-    return false;
-  };
-  const isTotalText = (index: number, array: string[]) => {
-    if (array[index] === "Yhteensä:") return true;
-    if (array[index - 1] === "Yhteensä:") return true;
-    if (array[index - 2] === "Yhteensä:") return true;
-
-    return false;
-  };
-  const pdfDataPreClearingToRows = pdfDataFiltered.filter(
-    (value, index, array) => {
-      if (cleaningSubjects.includes(value)) return false;
-      if (isDataDate(array[index])) return false;
-      if (isTotalText(index, array)) return false;
-      if (isPaginationNumber(index, array)) return false;
-
-      return true;
-    }
-  );
-
-  console.log("Step 4 - Pre-cleaning to rows:", pdfDataPreClearingToRows);
-
-  const rowTemplate = [
-    /^\d{1,2}.\d{1,2}.\d{4}$/,
-    /^.+$/,
-    /^\d+$/,
-    /^\d+$/,
-    /^.+$/,
-    /^\d+,?\d{0,}$/,
-    /^\d+,?\d{0,}$/,
-  ];
-
+const process = (pdfData: TextItem[]) => {
   const result = [];
-  let currentRow = [];
-  let currentColIndex = 0;
-  let pdfDataIndex = 0;
-  let correctCounter = 0;
+  let row = [];
 
-  console.log("Step 5 - format rows:");
-
-  while (pdfDataIndex < pdfDataPreClearingToRows.length) {
-    const currentPdfDataItem = pdfDataPreClearingToRows[pdfDataIndex];
-
-    const itemIsValid = rowTemplate[currentColIndex].test(currentPdfDataItem);
-
-    console.log(
-      "Current item:",
-      currentPdfDataItem,
-      ", regExp:",
-      rowTemplate[currentColIndex],
-      ", is valid:",
-      itemIsValid
-    );
-
-    currentRow.push(itemIsValid ? currentPdfDataItem : "--");
-
-    currentColIndex++;
-
-    if (itemIsValid) {
-      pdfDataIndex++;
-    }
-
-    if (currentColIndex >= rowTemplate.length) {
-      console.log("Row has formated", currentRow);
-      console.log("===================================");
-
-      correctCounter++;
-      result.push([String(correctCounter), ...currentRow]);
-
-      currentColIndex = 0;
-      currentRow = [];
-
-      if (!itemIsValid) {
-        //todo need to correct terminate condition and rows validation
-        console.log(
-          `An invalid row. ${result.length} rows have been processed`
-        );
-        break;
-      }
+  for (let item of pdfData) {
+    if (!item.hasEOL) {
+      row.push(item);
+    } else {
+      result.push(row);
+      row = [];
     }
   }
 
-  return result;
+  console.log(result);
 };
 
 const testDataHandler: DataHandler = async (fileRawData) => {
   const pdfData = await parse(fileRawData);
-
+  process(pdfData as TextItem[]);
   // console.log("Step 1 - raw PDF data:", pdfData);
 
   if (!pdfData) {
@@ -248,11 +38,10 @@ const testDataHandler: DataHandler = async (fileRawData) => {
       "Määrä",
       "Summa",
     ],
-    data: await process(
-      pdfData.filter(
-        (item: any) => item !== undefined && item !== " " && item !== ""
-      )
-    ),
+    data: [],
+    // pdfData.filter(
+    //   (item: any) => item !== undefined && item !== " " && item !== ""
+    // )
   };
 };
 
--- src/servicces/fileParsers/pdfParser.ts
+++ src/servicces/fileParsers/pdfParser.ts
@@ -1,16 +1,22 @@
 import * as pdfjsLib from "pdfjs-dist";
-import { PDFPageProxy, TextContent } from "pdfjs-dist/types/src/display/api";
+import {
+  PDFPageProxy,
+  TextContent,
+  TextItem,
+  TextMarkedContent,
+} from "pdfjs-dist/types/src/display/api";
 pdfjsLib.GlobalWorkerOptions.workerSrc = "pdf.worker.min.js";
 
-const parse = async (fileRawData: string): Promise<void | string[]> => {
+const parse = async (
+  fileRawData: string
+): Promise<void | (TextItem | TextMarkedContent)[]> => {
   const doc = await pdfjsLib.getDocument(fileRawData).promise;
-
-  let result: string[] = [];
+  let result: (TextItem | TextMarkedContent)[] = [];
 
   for (let pageNum = 1; pageNum <= doc.numPages; pageNum++) {
     const page: PDFPageProxy = await doc.getPage(pageNum);
     const textContent: TextContent = await page.getTextContent();
-    result = [...result, ...textContent.items.map((item: any) => item.str)];
+    result = [...result, ...textContent.items];
   }
 
   return result;

e6ef79306179d2f6f36a4054ea514dabbf32b24e
SMozhaev <mozhaev.dev@gmail.com>
2022-02-23 18:24:11 +0200

    test 2

--- src/servicces/dataHandlers/testDataHandler.ts
+++ src/servicces/dataHandlers/testDataHandler.ts
@@ -1,30 +1,90 @@
 import { DataHandler } from "../personal-data-parser";
 import parse from "../fileParsers/pdfParser";
-import { TextItem, TextMarkedContent } from "pdfjs-dist/types/src/display/api";
+import { TextItem } from "pdfjs-dist/types/src/display/api";
 
-const process = (pdfData: TextItem[]) => {
+const colNames = [
+  "Päivämäärä",
+  "Kauppa",
+  "Kuittinumero",
+  "Korttinumero",
+  "Tuote",
+  "Määrä",
+  "Summa sis. ALV",
+];
+
+const processRawData = (pdfData: TextItem[]): TextItem[][] => {
   const result = [];
   let row = [];
 
   for (let item of pdfData) {
-    if (!item.hasEOL) {
+    if (!["", " "].includes(item.str)) {
       row.push(item);
-    } else {
+    }
+
+    if (item.hasEOL) {
       result.push(row);
       row = [];
     }
   }
 
-  console.log(result);
+  return result;
+};
+
+const findTitleRow = (pdfData: TextItem[][]): TextItem[] | undefined => {
+  return pdfData.find((row) => {
+    let result = true;
+    for (let index in row) {
+      if (colNames[index] !== row[index].str) {
+        result = false;
+        break;
+      }
+    }
+    return result;
+  });
+};
+
+const format = (pdfData: TextItem[][], titleRow: TextItem[]) => {
+  return pdfData.reduce((acc: any, row) => {
+    const formatedRow: TextItem[][] = [[], [], [], [], [], [], []];
+    let currentColIndex = 0;
+    let currentItemIndex = 0;
+
+    while (currentColIndex <= 6 && currentItemIndex <= row.length - 1) {
+      const item = row[currentItemIndex];
+      if (
+        item.transform[4] >= titleRow[currentColIndex].transform[4] &&
+        (currentColIndex < titleRow.length - 1
+          ? item.transform[4] < titleRow[currentColIndex + 1].transform[4]
+          : true)
+      ) {
+        formatedRow[currentColIndex].push(item);
+        currentItemIndex++;
+      } else {
+        currentColIndex++;
+      }
+    }
+
+    acc.push(formatedRow);
+    return acc;
+  }, []);
 };
 
 const testDataHandler: DataHandler = async (fileRawData) => {
   const pdfData = await parse(fileRawData);
-  process(pdfData as TextItem[]);
-  // console.log("Step 1 - raw PDF data:", pdfData);
 
   if (!pdfData) {
     throw new Error("Invalid file data");
+  }
+
+  const processedRawData = processRawData(pdfData as TextItem[]);
+
+  console.log(processedRawData);
+
+  const titleRow = findTitleRow(processedRawData);
+
+  if (titleRow) {
+    const result = format(processedRawData, titleRow);
+    console.log(result);
   }
 
   return {
